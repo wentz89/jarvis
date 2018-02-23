7d28a94b9e93565da0df1d6d0270b4e8ca903164
oliver <o.uphus@gmail.com>
2018-02-23 16:15:16 +0100

    logig_simple

--- speech_navigation/src/logic_node.py
+++ speech_navigation/src/logic_node.py
@@ -5,66 +5,73 @@ from jarvis_msgs.msg import Command
 class PythonExample:
     def __init__(self):
         self.sub = rospy.Subscriber("/command", String, self.sub_callback)
-        self.pub = rospy.Publisher("/jarvis/command", Twist, queue_size = 10)
+        self.pub = rospy.Publisher("/jarvis/command", Command, queue_size = 10)
         self.timer = rospy.Timer(rospy.Duration(0.1),self.timer_callback)
         self.command = ""
         self.command_recived = False
+        self.current_time = rospy.get_rostime()
+        self.turn_recieved = False
+        self.enable = False
     
     def sub_callback(self, data):
         self.command = data.data
         self.command_recived = True
 
     def timer_callback(self, data):
-        turn_recieved = False
         if self.command == "jarvis":
-            enable = True
-            current_time = rospy.get_rostime()
-        if (rospy.get_rostime() - current_time >= 4.0):
-            enable = False
-        if enable:
+            self.enable = True
+            self.current_time = rospy.get_rostime()
+        
+        if (rospy.get_rostime() - self.current_time >= rospy.Duration(4.0)):
+            self.enable = False
+        if self.enable:
             if self.command_recived:
                 # === simple === #
                 simple_msg = Command()
-                simple_msg.command_type = 2
+                simple_msg.command_type.data = 2
                 
-                if not turn_recieved:
+                if not self.turn_recieved:
                     if self.command == "forward":
-                        simple_msg.arguments = "forward"
-                        enable = False
+                        simple_msg.arguments = ["forward"]
+                        self.enable = False
                     elif self.command == "back":
                         simple_msg.arguments = "back"
-                        enable = False
+                        self.enable = False
                     elif self.command == "stop":
                         simple_msg.arguments = "stop"
-                        enable = False
+                        self.enable = False
                     elif self.command == "left":
-                        simple_msg.arguments = "left"
-                        enable = False
+                        simple_msg.arguments = ["left"]
+                        self.enable = False
                     elif self.command == "right":
                         simple_msg.arguments = "right"
-                        enable = False
+                        self.enable = False
                     elif self.command == "turn":
-                        turn_recieved = True
+                        self.turn_recieved = True
                         current_time = rospy.get_rostime()
                 
-                if turn_recieved:
+                if self.turn_recieved:
                     if self.command == "left":
                         simple_msg.arguments = ["turn","left"]
-                        enable = False
+                        self.enable = False
+                        self.turn_recieved = False
                     elif self.command == "right":
                         simple_msg.arguments = ["turn","right"]
-                        enable = False
+                        self.enable = False
+                        self.turn_recieved = False
                     elif self.command == "stop":
                         simple_msg.arguments = ["turn","stop"]
-                        enable = False
+                        self.enable = False
+                        self.turn_recieved = False
 
-                self.pub.publish(simple_msg)
+                if not self.enable:
+                    self.pub.publish(simple_msg)
 
     def run(self,rate):
         rate.sleep()
 
 def main():
-    rospy.init_node('Test')
+    rospy.init_node('Logic')
     
     t = PythonExample()
     
