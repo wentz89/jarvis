898535400e8af8b9ea789a8ed902c793c81be3d1
oliver <o.uphus@gmail.com>
2018-02-23 21:19:18 +0100

    logic_simple_update

--- speech_navigation/src/logic_node.py
+++ speech_navigation/src/logic_node.py
@@ -18,37 +18,38 @@ class PythonExample:
         self.command_recived = True
 
     def timer_callback(self, data):
-        if self.command == "jarvis":
-            self.enable = True
-            self.current_time = rospy.get_rostime()
-        
-        if (rospy.get_rostime() - self.current_time >= rospy.Duration(4.0)):
+        if (rospy.get_rostime() - self.current_time >= rospy.Duration(4.0) and self.enable):
             self.enable = False
-        if self.enable:
-            if self.command_recived:
+            print "too slow"
+        if self.command_recived:
+            if self.command == "jarvis":
+                self.enable = True
+                self.current_time = rospy.get_rostime()
+            if self.enable:
+
                 # === simple === #
                 simple_msg = Command()
-                simple_msg.command_type.data = 2
+                simple_msg.command_type = 2
                 
                 if not self.turn_recieved:
                     if self.command == "forward":
                         simple_msg.arguments = ["forward"]
                         self.enable = False
                     elif self.command == "back":
-                        simple_msg.arguments = "back"
+                        simple_msg.arguments = ["back"]
                         self.enable = False
                     elif self.command == "stop":
-                        simple_msg.arguments = "stop"
+                        simple_msg.arguments = ["stop"]
                         self.enable = False
                     elif self.command == "left":
                         simple_msg.arguments = ["left"]
                         self.enable = False
                     elif self.command == "right":
-                        simple_msg.arguments = "right"
+                        simple_msg.arguments = ["right"]
                         self.enable = False
                     elif self.command == "turn":
                         self.turn_recieved = True
-                        current_time = rospy.get_rostime()
+                        self.current_time = rospy.get_rostime()
                 
                 if self.turn_recieved:
                     if self.command == "left":
@@ -66,6 +67,7 @@ class PythonExample:
 
                 if not self.enable:
                     self.pub.publish(simple_msg)
+        self.command_recived = False
 
     def run(self,rate):
         rate.sleep()
