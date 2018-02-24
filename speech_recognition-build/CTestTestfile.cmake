7bb736b3cb7ddd14958c6a5a05b91b009264a42a
oliver <o.uphus@gmail.com>
2018-02-24 12:15:39 +0100

    ...

--- speech_recognition-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shaoxuan/catkin_ws/src/jarvis/speech_recognition
-# Build directory: /home/shaoxuan/catkin_ws/src/jarvis/speech_recognition-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
--- speech_recognition-build/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/shaoxuan/catkin_ws/src/jarvis/speech_recognition-build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
